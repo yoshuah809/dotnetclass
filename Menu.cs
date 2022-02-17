334e5863be1f8139e2bd54c4bcd060ba01a4a2a1
Josue Garcia <yoshuah809@gmail.com>
2022-02-17 12:17:09 -0600

    Program instantiation

---
+++ Menu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace netCoreClass
+{
+    public class Menu
+    {
+        public void getProduct()
+        {
+            Console.WriteLine("C# Class");
+        }
+    }
+}
